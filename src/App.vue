068e62596bc258b9eab26627f2573078040c0f35
VincenzoDePetris <vince02@live.it>
2023-09-07 16:24:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello World",
+      image: "./assets/img/dog.jpg",
+    };
+  },
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL(imageName, import.meta.url);
+      console.log(imageUrl);
+      return imageUrl.href;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <div><img :src="buildImagePath(image)" alt="" /></div>
+</template>
+
+<style></style>
